943446cf64b0dc951b7703907272a436af6cef97
Vikesh115 <vikeshraut952@gmail.com>
2025-02-12 12:59:10 +0530

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,12 +9,15 @@ function Navbar() {
 
   return (
     <div className='flex w-[100%] h-[6%] justify-evenly items-center shadow-2xl py-4'>
-      <NavLink to='/' className='flex justify-between gap-2 text-blue-400 items-center'>
+      <NavLink to='/' className='lg:flex hidden justify-between gap-2 text-blue-400 items-center'>
         <span>W O R L D</span>
         <span className="bg-green-950 text-white p-2 items-center italic font-medium rounded-xl">of</span>
         <span>H O T E L</span>
       </NavLink>
-      <div className='flex gap-5'>
+      <NavLink to='/' className='lg:hidden flex justify-between gap-2 text-blue-400 items-center'>
+        <span>H O T E L</span>
+      </NavLink>
+      <div className='flex gap-1 lg:gap-5'>
         <NavLink
           to="/hotel"
           style={({ isActive }) => ({
@@ -23,7 +26,7 @@ function Navbar() {
           className="flex gap-1 flex-col items-center"
         >
           <span><RiHotelLine size={20} /></span>
-          <span className='font-thin'>Hotel</span>
+          <span className='font-thin font-stretch-50% lg:font-stretch-100%'>Hotel</span>
         </NavLink>
         <NavLink
           to="/train"
@@ -33,7 +36,7 @@ function Navbar() {
           className="flex gap-1 flex-col items-center"
         >
           <span><SiRailway size={20} /></span>
-          <span className='font-thin'>Train</span>
+          <span className='font-thin font-stretch-50% lg:font-stretch-100%'>Train</span>
         </NavLink>
         <NavLink
           to="/bus"
@@ -43,10 +46,10 @@ function Navbar() {
           className="flex gap-1 flex-col items-center"
         >
           <span><FaBus size={20} /></span>
-          <span className='font-thin'>Buses</span>
+          <span className='font-thin font-stretch-50% lg:font-stretch-100%'>Buses</span>
         </NavLink>
       </div>
-      <div className='lg:flex hidden items-center py-1 px-3 rounded-sm bg-gradient-to-r from-blue-400 to-blue-600 text-white'>
+      <div className='flex items-center py-1 px-3 rounded-sm bg-gradient-to-r from-blue-400 to-blue-600 text-white'>
         <NavLink onClick={notify}>Blog</NavLink>
         <ToastContainer
           position="top-center"
