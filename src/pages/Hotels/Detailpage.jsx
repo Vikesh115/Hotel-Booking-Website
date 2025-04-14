a24bb884ba89d3bb6e5f0c63081907cca6cdff9a
Vikesh115 <vikeshraut952@gmail.com>
2025-04-14 13:05:40 +0530

    updated

--- src/pages/Hotels/Detailpage.jsx
+++ src/pages/Hotels/Detailpage.jsx
@@ -8,7 +8,7 @@ import { FaRegThumbsUp } from "react-icons/fa";
 import Facility from './facility/Facility';
 import Type from './rooms/Type';
 import Footer from './footer/Footer';
-import Details from './Home/Details';
+import Details from './home/Details';
 
 function Detailpage() {
     const { details, loading } = useSelector((state) => state.hotels)
